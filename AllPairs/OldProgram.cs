a9fa5ad60b2da41b6a221740f3a2da8ac1e9681a
GET Academy <terje@kolderup.net>
2024-06-12 08:30:15 +0200

    Create OldProgram.cs

---
+++ AllPairs/OldProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace AllPairs
+{
+    internal class OldProgram
+    {
+        public static void Mainx(string[] args)
+        {
+            if (args.Length < 2) return;
+
+            var indexA = 0;
+            var indexB = 1;
+            var lastIndex = args.Length - 1;
+
+            while (indexA <= lastIndex && indexB <= lastIndex)
+            {
+                Console.WriteLine($"{args[indexA]} {args[indexB]}");
+                do
+                {
+                    indexB++;
+                    if (indexB > lastIndex)
+                    {
+                        indexA++;
+                        indexB = indexA + 1;
+                    }
+                } while (indexB == indexA);
+            }
+        }
+    }
+}
