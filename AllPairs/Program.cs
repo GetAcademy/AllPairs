302eb5960087fac2ba0491dfc26480373b6565a1
GET Academy <terje@kolderup.net>
2024-06-12 08:28:55 +0200

    Initial commit

---
+++ AllPairs/Program.cs
@@ -0,0 +1,19 @@
+﻿if (args.Length < 2) return;
+
+var indexA = 0;
+var indexB = 1;
+var lastIndex = args.Length - 1;
+
+while (indexA <= lastIndex && indexB <= lastIndex)
+{
+    Console.WriteLine($"{args[indexA]} {args[indexB]}");
+    do
+    {
+        indexB++;
+        if (indexB > lastIndex)
+        {
+            indexA++;
+            indexB = 0;
+        }
+    } while (indexB == indexA);
+}
